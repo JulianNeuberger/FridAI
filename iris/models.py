377baca8db3b3de332c432f99a42a35024283aeb
Julian Neuberger <neuberger.julian@gmail.com>
2018-03-20 13:39:35 +0100

    remove working model, so it doesnt spoil the task

--- iris/models.py
+++ iris/models.py
@@ -8,14 +8,10 @@ def get_model() -> Model:
     # here starts your task!
     # implement an ANN that solves the iris task
     # (solving means it is better than guessing, which is correct 33% of the time)
-    input_layer = Input(shape=(FEATURE_DIMENSIONALITY,))
-    hidden_layer = Dense(units=256, activation='relu', use_bias=True)(input_layer)
-    hidden_layer = Dense(units=128, activation='relu', use_bias=True)(hidden_layer)
-    hidden_layer = Dense(units=64, activation='relu', use_bias=True)(hidden_layer)
-    output_layer = Dense(units=NUM_CLASSES, activation='softmax')(hidden_layer)
+
 
     # create a new model by specifying input/output layer(s)
-    model = Model(inputs=[input_layer], outputs=[output_layer])
+    model = Model(inputs=[], outputs=[])
     # I already chose optimizer and loss function, you won't need to teak them (but you can of course!)
     optimizer = Adam()
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])

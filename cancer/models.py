b5479d16d262a14074bac2622d6f34e5e6b1e64f
Julian Neuberger <neuberger.julian@gmail.com>
2018-03-21 12:40:38 +0100

    removed solution

--- cancer/models.py
+++ cancer/models.py
@@ -10,21 +10,10 @@ def get_model() -> Model:
     # implement an ANN that solves the cancer task
     # (solving means it is better than guessing, which is correct 50% of the time)
     # delete the following (single) line and replace it with your model
-    #raise NotImplementedError('You have to implement {}.get_model'.format(os.path.basename(__file__)))
-
-    input_layer = Input(shape=(FEATURE_DIMENSIONALITY,))
-    hidden_layer = Dense(units=100, activation='relu')(input_layer)
-    hidden_layer = Dense(units=100, activation='relu')(hidden_layer)
-    hidden_layer = Dense(units=100, activation='relu')(hidden_layer)
-    hidden_layer = Dense(units=100, activation='relu')(hidden_layer)
-    hidden_layer = Dense(units=50, activation='relu')(hidden_layer)
-    hidden_layer = Dense(units=50, activation='relu')(hidden_layer)
-    hidden_layer = Dense(units=50, activation='relu')(hidden_layer)
-    hidden_layer = Dense(units=50, activation='relu')(hidden_layer)
-    output_layer = Dense(units=NUM_CLASSES, activation='softmax')(hidden_layer)
+    raise NotImplementedError('You have to implement {}.get_model'.format(os.path.basename(__file__)))
 
     # create a new model by specifying input/output layer(s)
-    model = Model(inputs=[input_layer], outputs=[output_layer])
+    model = Model(inputs=[], outputs=[])
     # I already chose optimizer and loss function, you won't need to tweak them (but you can of course!)
     optimizer = Adam()
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
